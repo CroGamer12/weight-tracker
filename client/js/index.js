9a1c71820f10b2807a85b4048cdfb5c748a67b5a
CroGamer12 <48877715+CroGamer12@users.noreply.github.com>
2019-08-23 12:54:37 +0200

    added a cart

--- client/js/index.js
+++ client/js/index.js
@@ -1,18 +1,110 @@
 import '../scss/index.scss';
+import Chart from 'chart.js';
 
-fetch(`http://localhost:3000/add`, {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+// fetch(`http://localhost:3000/edit`, {
+//     method: 'PUT',
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         old: {
+//             name: "cro"
+//         },
+//         new: {
+//             name: "cro1"
+//         }
+//     })
+// })
+//     .then(res => {
+//         return res.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+
+// fetch(`http://localhost:3000/add`, {
+//     method: 'POST',
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         name: "cro"
+//     })
+// })
+//     .then(res => {
+//         return res.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+
+// fetch(`http://localhost:3000/remove`, {
+//     method: "DELETE",
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         name: "cro1"
+//     })
+// })
+//     .then(res => {
+//         return res.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+
+let draw = Chart.controllers.line.prototype.draw;
+Chart.controllers.line = Chart.controllers.line.extend({
+    draw: function () {
+        draw.apply(this, arguments);
+        let ctx = this.chart.chart.ctx;
+        let _stroke = ctx.stroke;
+        ctx.stroke = function () {
+            ctx.save();
+            ctx.shadowColor = '#E56590';
+            ctx.shadowBlur = 10;
+            ctx.shadowOffsetX = 0;
+            ctx.shadowOffsetY = 4;
+            _stroke.apply(this, arguments)
+            ctx.restore();
+        }
+    }
+});
+
+
+var ctx = document.getElementById('weightChart');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Yellow', 'Blue', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'KG',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 255, 255, 0)'
+            ],
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 2.5
+        }]
     },
-    body: JSON.stringify({
-        name: "cro"
-    })
-})
-    .then(res => {
-        return res.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        tooltips: {
+            mode: 'point'
+        },
+        title: {
+            display: true,
+            text: 'Weight Tracker'
+        }
+    }
+});
--- server/index.js
+++ server/index.js
@@ -11,17 +11,44 @@ app.use(express.json());
 app.get('/weight', (req, res) => {
     db.find({}, (err, data) => {
         if (err) {
-            response.end();
+            res.end();
             return;
         }
         res.json(data);
     })
 });
 
+app.put('/edit', (req, res) => {
+    console.log(req.body);
+    db.update(req.body.old, req.body.new, (err, num) => {
+        if (err) {
+            res.end();
+            return;
+        }
+        res.json(req.body);
+    });
+});
+
+app.delete('/remove', (req, res) => {
+    console.log(req.body);
+    db.remove(req.body, (err, num) => {
+        if (err) {
+            res.end();
+            return;
+        }
+        res.json(req.body);
+    });
+});
+
 app.post('/add', (req, res) => {
     console.log(req.body);
-    db.insert(req.body);
-    res.json(req.body);
+    db.insert(req.body, (err, num) => {
+        if (err) {
+            res.end();
+            return;
+        }
+        res.json(req.body);
+    });
 });
 
 const PORT = 3000 || process.env.PORT;
